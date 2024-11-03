d435433994ae1a13db46535d6b0953247a0fb29d
María Emilia Ramírez <mariaemiliaramirezgomez@comunidad.unam.mx>
2024-11-03 23:08:09 +0000

    add joycons and bluetooth methods in pyhton

---
+++ modules/joycons.py
@@ -0,0 +1,37 @@
+import time
+import subprocess
+import json
+import bluetooth  
+
+class joycons:
+
+    def __init__(self):
+        self.pathScriptGetMac = "../scripts/bluetooth-get-mac-joycons"
+        self.joyconsMac = {}
+
+    def getMacJoyCons(self):
+        result = subprocess.run([self.pathScriptGetMac], capture_output=True, text=True, check=True)
+        self.joyconsMac = json.loads(result.stdout)
+        
+        return
+
+    def connectJoyCons(self):
+
+        self.getMacJoyCons()
+        bt = bluetooth.bluetooth()
+
+        for joycon, mac in self.joyconsMac.items(): 
+            bt.connectDevice(mac)
+        
+        bt.close()
+        return 
+
+    def disconnectJoyCons(self):
+        bt = bluetooth.bluetooth()
+
+        for joycon, mac in self.joyconsMac: 
+            bt.disconnectDevice(mac)
+            bt.removeDevice(mac)
+        
+        bt.close()
+        return 
---
+++ scripts/bluetooth-get-mac-joycons.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+bluetoothctl power on
+bluetoothctl scan on > /dev/null &
+SCAN_PID=$!
+
+
+macJoyConL=""
+macJoyConR=""
+
+while [ -z "$macJoyConR" ] && [ -z "$macJoyConL" ]; do
+    macJoyConR=$(bluetoothctl devices | grep "Joy-Con (R)" | awk '{print $2}')
+    macJoyConL=$(bluetoothctl devices | grep "Joy-Con (L)" | awk '{print $2}')
+    sleep 1
+done
+
+kill $SCAN_PID
+
+echo "{\"macJoyConR\": \"$macJoyConR\", \"macJoyConL\": \"$macJoyConL\"}"
+
